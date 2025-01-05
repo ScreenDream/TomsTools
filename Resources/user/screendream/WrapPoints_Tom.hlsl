3be01c6d33e5f74c626ca49f7e2e25c53308dcbb
Thomas Helzle <113698935+ScreenDream@users.noreply.github.com>
2025-01-05 17:56:28 +0100

    Added spherical mode

--- Resources/user/screendream/WrapPoints_Tom.hlsl
+++ Resources/user/screendream/WrapPoints_Tom.hlsl
@@ -4,7 +4,7 @@
 cbuffer Params : register(b1)
 {
     float3 Position;
-    float __padding;
+    float FieldMode;
     float3 Size;
     float WrapMode;
 }
@@ -23,21 +23,69 @@ void main(uint3 i : SV_DispatchThreadID)
 
     ResultPoints[i.x] =  SourcePoints[i.x];
 
-    float3 a = (SourcePoints[i.x].Position - Position  + Size/2);
+    float3 pos = SourcePoints[i.x].Position;
     float3 newPosition;
-    
-    switch( (int)WrapMode) {
-        case 0: // Wrap
-            newPosition  =  mod(a, Size) - Size/2 + Position;
-            break;
-        case 1: // Clamp
-            newPosition  =  clamp(a, 0, Size) - Size/2 + Position;
-            break;
-        case 2: // Mirror
-            newPosition  =  mod(a, Size * 2.0);
-            newPosition = newPosition <= Size ? newPosition - Size/2 + Position : Size - newPosition + Size/2 + Position;
-            break;
+
+    if(FieldMode < 0.5)  // Box
+    {
+        float3 a = (pos - Position  + Size/2);
+        switch((int)WrapMode) 
+        {
+            case 0: // Wrap
+                newPosition  =  mod(a, Size) - Size/2 + Position;
+                break;
+            case 1: // Clamp
+                newPosition  =  clamp(a, 0, Size) - Size/2 + Position;
+                break;
+            case 2: // Mirror
+                newPosition  =  mod(a, Size * 2.0);
+                newPosition = newPosition <= Size ? newPosition - Size/2 + Position : Size - newPosition + Size/2 + Position;
+                break;
+        }        
     }
+    else // Sphere
+    {
+        float3 pos2 = pos - Position;
+        float dist = length(pos2);
+        float rad = Size.x * 0.5;
+        switch((int)WrapMode) 
+        {
+            case 0: // Wrap
+                if(dist > rad) 
+                {
+                    newPosition = normalize(pos2) * (rad - Size.x + fmod(dist + rad, Size.x)) + Position;
+                }
+                else
+                {
+                    newPosition = pos;
+                }
+                break;
+            case 1: // Clamp
+                if(dist > rad) 
+                {
+                    newPosition = normalize(pos2) * rad + Position;
+                }
+                else
+                {
+                    newPosition = pos;
+                }
+                break;
+            case 2: // Mirror
+                if(dist > rad) 
+                {
+                    float temp = fmod(dist + rad, Size.x * 2);
+                    temp = temp > Size.x ? Size.x - temp : temp - Size.x;
+                    newPosition = normalize(pos2) * (rad + temp) + Position;                
+                }
+                else
+                {
+                    newPosition = pos;
+                }
+                break;
+        }
+    }
+    
+
 
     ResultPoints[i.x].Position = newPosition;
 }
